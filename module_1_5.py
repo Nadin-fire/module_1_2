bfaf66d47f576bfe879e928f5fca328c5e483c53
Nadin-fire <ioyanvm@gmail.com>
2024-09-29 00:59:30 +0800

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2, 3, 'apple', True
+print(immutable_var)
+mutable_list = [1, 2, 3, 'apple', False]
+mutable_list [3] = 'peach'
+print(mutable_list)
+immutable_var[1] = 5
+print(immutable_var) # кортеж - это неизменяемая коллекция
